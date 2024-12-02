22cee66e1f698d0e436de42c26187758f3b0d58a
Kim Birkelund <kim@birkelund.me>
2024-12-02 15:28:41 +0100

    Solve Day2-b.

--- src/Day2/Program.cs
+++ src/Day2/Program.cs
@@ -5,18 +5,27 @@ var reports = File.ReadAllLines("input.txt")
                                       .Select(int.Parse)
                                       .ToList());
 
+bool IsReportSafe(IReadOnlyList<int> report)
+{
+    var isReportSafe = report.Zip(report.Skip(1), (x, y) => (x - y) switch
+                             {
+                                 >= 1 and <= 3 => 1,
+                                 <= -1 and >= -3 => -1,
+                                 _ => 0
+                             })
+                             .Sum() is { } risingOrDecreasingTerms && Math.Abs(risingOrDecreasingTerms) == report.Count - 1;
+    Console.WriteLine($"   {string.Join(", ", report)}: {isReportSafe}");
+    return isReportSafe;
+}
+
 var safeReports = reports
-                  .Where(report =>
-                         {
-                             return report.Zip(report.Skip(1), (x, y) => (x - y) switch
-                                          {
-                                              >= 1 and <= 3 => 1,
-                                              <= -1 and >= -3 => -1,
-                                              _ => 0
-                                          })
-                                          .Sum() is { } risingOrDecreasingTerms
-                                    && Math.Abs(risingOrDecreasingTerms) == report.Count - 1;
-                         })
-                  .Count();
+    .Count(r =>
+           {
+               Console.WriteLine($"{string.Join(", ", r)}:");
+               return IsReportSafe(r)
+                      || Enumerable.Range(0, r.Count)
+                                   .Select(i => (int[]) [..r.Take(i), ..r.Skip(i + 1)])
+                                   .Any(IsReportSafe);
+           });
 
 Console.WriteLine($"SafeReports: {safeReports}");
