87ee959bbfd00928f7a2e962e41ace9ecbcbdb64
Kim Birkelund <kim@birkelund.me>
2024-12-03 15:42:37 +0100

    Solve Day3-a.

---
+++ src/Day3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var input = File.ReadAllText("input.txt");
+
+var regex = new Regex(@"mul\((?<op1>\d+),(?<op2>\d+)\)");
+
+var result = regex.Matches(input)
+                  .Select(m => int.Parse(m.Groups["op1"].Value) * int.Parse(m.Groups["op2"].Value))
+                  .Sum();
+
+Console.WriteLine(result);
