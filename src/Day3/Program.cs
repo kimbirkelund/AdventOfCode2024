689f7621c3aeb35422384e9de1aa88d0c240b0a0
Kim Birkelund <kim@birkelund.me>
2024-12-04 07:02:30 +0100

    Solve Day3-b.

--- src/Day3/Program.cs
+++ src/Day3/Program.cs
@@ -4,10 +4,22 @@ using System.Text.RegularExpressions;
 
 var input = File.ReadAllText("input.txt");
 
-var regex = new Regex(@"mul\((?<op1>\d+),(?<op2>\d+)\)");
+var regex = new Regex(@"(?<do>do\(\))|(?<dont>don't\(\))|mul\((?<op1>\d+),(?<op2>\d+)\)");
 
-var result = regex.Matches(input)
-                  .Select(m => int.Parse(m.Groups["op1"].Value) * int.Parse(m.Groups["op2"].Value))
-                  .Sum();
+var nextMultEnabled = true;
+var result = 0;
+
+foreach (Match match in regex.Matches(input))
+{
+    if (match.Groups["do"].Success)
+        nextMultEnabled = true;
+    else if (match.Groups["dont"].Success)
+        nextMultEnabled = false;
+    else
+    {
+        if (nextMultEnabled)
+            result += int.Parse(match.Groups["op1"].Value) * int.Parse(match.Groups["op2"].Value);
+    }
+}
 
 Console.WriteLine(result);
