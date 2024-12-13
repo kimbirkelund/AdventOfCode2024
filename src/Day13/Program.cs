7197aa2e89ae3456c7999ffa85e9eff618921571
Kim Birkelund <kim@birkelund.me>
2024-12-13 16:47:22 +0100

    Solve Day13-b.

--- src/Day13/Program.cs
+++ src/Day13/Program.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Immutable;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 var regex = new Regex(@"^(Button (?<button>A|B): X\+(?<x>\d+), Y\+(?<y>\d+)|Prize: X=(?<x>\d+), Y=(?<y>\d+))$");
-var machines = new List<(Button[] buttons, (int x, int y) prizePosition)>();
+var machines = new List<(Button[] buttons, (long x, long y) prizePosition)>();
+
+var offset = 10000000000000;
 foreach (var lines in File.ReadAllLines("input.txt").Chunk(4))
 {
     var buttons = new List<Button>();
@@ -13,11 +14,16 @@ foreach (var lines in File.ReadAllLines("input.txt").Chunk(4))
         {
             if (groups["button"] is { Success: true, Value: { } buttonType })
             {
-                buttons.Add(new Button(GetButtonPrice(buttonType), int.Parse(groups["x"].Value), int.Parse(groups["y"].Value)));
+                buttons.Add(new Button(GetButtonPrice(buttonType),
+                                       int.Parse(groups["x"].Value),
+                                       int.Parse(groups["y"].Value)));
             }
             else
             {
-                machines.Add((buttons.ToArray(), (int.Parse(groups["x"].Value), int.Parse(groups["y"].Value))));
+                machines.Add((buttons.ToArray(),
+                                 (
+                                     long.Parse(groups["x"].Value) + offset,
+                                     long.Parse(groups["y"].Value) + offset)));
             }
         }
     }
@@ -25,40 +31,26 @@ foreach (var lines in File.ReadAllLines("input.txt").Chunk(4))
 
 var cheapestPrices = machines.Select(m => FindCheapestPrice(m.buttons, m.prizePosition))
                              .Where(v => v.HasValue)
-                             .Cast<int>()
+                             .Cast<long>()
                              .ToArray();
 
 Console.WriteLine($"Prize count: {cheapestPrices.Count()}");
 Console.WriteLine($"Cheapest price: {cheapestPrices.Sum()}");
 
-int? FindCheapestPrice(IReadOnlyCollection<Button> buttons, (int x, int y) prizePosition)
+long? FindCheapestPrice(IReadOnlyList<Button> buttons, (long x, long y) prizePosition)
 {
-    var cache = ImmutableDictionary<(int, int), int?>.Empty;
+    var A = buttons[0];
+    var B = buttons[1];
 
-    return FindCheapestPriceImpl(prizePosition, ImmutableDictionary<Button, int>.Empty);
+    var b = (prizePosition.y * A.X - prizePosition.x * A.Y) / (double)(A.X * B.Y - B.X * A.Y);
+    var a = (prizePosition.x - b * B.X) / A.X;
 
-    int? FindCheapestPriceImpl((int x, int y) distanceToPrize, ImmutableDictionary<Button, int> presses)
-    {
-        if (cache.TryGetValue(distanceToPrize, out var cheapestPrice))
-            return cheapestPrice;
+    if (!double.IsInteger(a) || !double.IsInteger(b))
+        return null;
 
-        cheapestPrice = buttons.Select(b =>
-                                       {
-                                           if (presses.TryGetValue(b, out var pressCount) && pressCount >= 100)
-                                               return null;
-                                           var localPresses = presses.SetItem(b, pressCount + 1);
-                                           return b.Press(distanceToPrize) switch
-                                           {
-                                               (0, 0) => b.Price,
-                                               { } newDistanceToPrize => b.Price + FindCheapestPriceImpl(newDistanceToPrize, localPresses),
-                                               _ => null
-                                           };
-                                       })
-                               .Min();
+    var bestPrice = a * A.Price + b * B.Price;
 
-        ImmutableInterlocked.TryAdd(ref cache, distanceToPrize, cheapestPrice);
-        return cheapestPrice;
-    }
+    return (long)bestPrice;
 }
 
 int GetButtonPrice(string buttonType)
@@ -71,7 +63,7 @@ int GetButtonPrice(string buttonType)
 
 internal record Button(int Price, int X, int Y)
 {
-    public (int x, int y)? Press((int x, int y) target)
+    public (long x, long y)? Press((long x, long y) target)
     {
         var newX = target.x - X;
         var newY = target.y - Y;
