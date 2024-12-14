1569eb384915f2c00d87b9afd9ea242e719b124e
Kim Birkelund <kim@birkelund.me>
2024-12-14 19:01:59 +0100

    Solve Day14-b.

--- src/Day14/Program.cs
+++ src/Day14/Program.cs
@@ -7,18 +7,70 @@ var robots = input.Select(Robot.Parse)
 Console.WriteLine("Robots:");
 Console.WriteLine(string.Join('\n', robots.Select(r => $"  {r}")));
 
-PrintFloorMap();
-foreach (var _ in Enumerable.Range(0, 100))
-{
-    foreach (var robot in robots)
-        robot.MoveOnce();
+var seconds = 0;
+var speed = 0;
+var stepOne = 0;
 
+
+var waitHandle = new AutoResetEvent(initialState: false);
+
+Task.Run(() =>
+         {
+             while (true)
+             {
+                 var key = Console.ReadKey();
+
+                 if (key.Key == ConsoleKey.UpArrow)
+                     speed += key.Modifiers.HasFlag(ConsoleModifiers.Control) ? 10 : 1;
+                 else if (key.Key == ConsoleKey.DownArrow)
+                     speed -= key.Modifiers.HasFlag(ConsoleModifiers.Control) ? 10 : 1;
+                 else if (key.Key == ConsoleKey.Spacebar)
+                     speed = 0;
+                 else if (key.Key == ConsoleKey.LeftArrow)
+                 {
+                     speed = 0;
+                     stepOne = -1;
+                 }
+                 else if (key.Key == ConsoleKey.RightArrow)
+                 {
+                     speed = 0;
+                     stepOne = 1;
+                 }
+
+                 waitHandle.Set();
+             }
+         });
+
+Console.Clear();
+
+while (true)
+{
+    var actualSpeed = speed + stepOne;
+    if (actualSpeed != 0)
+    {
+        foreach (var robot in robots)
+        {
+            if (actualSpeed < 0)
+                robot.MoveBackward();
+            else
+                robot.MoveForward();
+        }
+
+        seconds += actualSpeed < 0 ? -1 : 1;
+    }
+
+    stepOne = 0;
+
+    Console.SetCursorPosition(0, 0);
+    Console.WriteLine($"Seconds: {seconds}, speed: {speed}".PadRight(Robot.AreaWidth, ' '));
     PrintFloorMap();
     Console.WriteLine();
+
+    waitHandle.WaitOne(speed == 0 ? Timeout.Infinite : 1000 / Math.Abs(speed));
 }
 
-var safetyScore = robots.Where(r => r.Quardrant != null)
-                        .GroupBy(r => r.Quardrant)
+var safetyScore = robots.Where(r => r.Quadrant != null)
+                        .GroupBy(r => r.Quadrant)
                         .Select(g => g.Count())
                         .Aggregate((product, next) => product * next);
 Console.WriteLine($"Safety score: {safetyScore}");
@@ -43,15 +95,14 @@ void PrintFloorMap()
 public class Robot
 {
     private const int AreaHeight = 103;
-    private const int AreaMiddleX = AreaWidth / 2;
+    public const int AreaMiddleX = AreaWidth / 2;
     private const int AreaMiddleY = AreaHeight / 2;
-    private const int AreaWidth = 101;
-
+    public const int AreaWidth = 101;
     public static readonly int[,] FloorMap = new int[AreaWidth, AreaHeight];
     private static readonly Regex _robotRegex = new(@"^p=(?<x>\d+),(?<y>\d+) v=(?<vx>-?\d+),(?<vy>-?\d+)$");
 
 
-    public int? Quardrant
+    public int? Quadrant
         => (X, Y) switch
         {
             (AreaMiddleX, _) => null,
@@ -62,12 +113,13 @@ public class Robot
             (> AreaMiddleX, > AreaMiddleY) => 4
         };
 
-    public int VelocityX { get; }
-    public int VelocityY { get; }
-    public int X { get; private set; }
-    public int Y { get; private set; }
+    public int X { get; set; }
+    public int Y { get; set; }
 
-    public Robot(int x, int y, int velocityX, int velocityY)
+    private int VelocityX { get; }
+    private int VelocityY { get; }
+
+    private Robot(int x, int y, int velocityX, int velocityY)
     {
         VelocityX = velocityX;
         VelocityY = velocityY;
@@ -76,7 +128,17 @@ public class Robot
         FloorMap[X, Y]++;
     }
 
-    public void MoveOnce()
+    public void MoveBackward()
+    {
+        FloorMap[X, Y]--;
+
+        X = (X - VelocityX + AreaWidth) % AreaWidth;
+        Y = (Y - VelocityY + AreaHeight) % AreaHeight;
+
+        FloorMap[X, Y]++;
+    }
+
+    public void MoveForward()
     {
         FloorMap[X, Y]--;
 
@@ -87,7 +149,17 @@ public class Robot
     }
 
     public override string ToString()
-        => $"p={X},{Y} v={VelocityX},{VelocityY} q={Quardrant}";
+        => $"p={X},{Y} v={VelocityX},{VelocityY} q={Quadrant}";
+
+    public static int FloorAt(int centerX, int centerY)
+    {
+        if (centerX < 0 || centerX >= AreaWidth)
+            return -1;
+        if (centerY < 0 || centerY >= AreaHeight)
+            return -1;
+
+        return FloorMap[centerX, centerY];
+    }
 
     public static Robot Parse(string input)
     {
